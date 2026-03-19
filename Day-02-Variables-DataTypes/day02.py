cfc3723ec36c0c581df713b4e81f43321540c52f
Faizan Ali <fali@simplelogix.com>
2026-03-19 16:08:44 -0500

    Day-02 : Python - Variables & Data Types Practice

---
+++ Day-02-Variables-DataTypes/day02.py
@@ -0,0 +1,35 @@
+employee_name= "Faisal Mustafa"
+
+company = "Simplelogix"
+
+years_experience = 10
+
+hourly_rate = 75.50
+
+is_available = True
+
+
+
+print(employee_name)
+
+print(company)
+
+print(years_experience)
+
+print(hourly_rate)
+
+print(is_available)
+
+
+
+
+
+print(type(employee_name))
+
+print(type(company))
+
+print(type(years_experience))
+
+print(type(hourly_rate))
+
+print(type(is_available))
